8f751160fa7e465829c62a7ee933e5b915403be7
Jeff Bluemel <jeffbluemel@gmail.com>
2026-01-11 10:58:07 -0600

    Add fy26 seed data and weekly metrics gold tables

---
+++ models/marts/weekly_metrics_summary.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='table') }}
+
+with weekly_data as (
+    select
+        -- Fiscal year calculation (Aug 1 = start of FY)
+        case 
+            when extract(month from auction_date) >= 8 
+            then extract(year from auction_date) + 1
+            else extract(year from auction_date)
+        end as fiscal_year,
+        
+        -- Fiscal week calculation (week 1 starts Aug 1)
+        case
+            when extract(month from auction_date) >= 8
+            then floor((auction_date - make_date(extract(year from auction_date)::int, 8, 1)) / 7) + 1
+            else floor((auction_date - make_date(extract(year from auction_date)::int - 1, 8, 1)) / 7) + 1
+        end as fiscal_week_number,
+        
+        contract_price,
+        item_id
+    from {{ ref('items') }}
+    where safe_for_alv = true
+      and is_sold = true
+)
+
+select
+    fiscal_year,
+    fiscal_week_number,
+    count(*) as total_items_sold,
+    round(avg(contract_price)::numeric, 2) as avg_lot_value,
+    sum(contract_price) as total_contract_price
+from weekly_data
+group by fiscal_year, fiscal_week_number
+order by fiscal_year, fiscal_week_number
--- models/staging/stg_items.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    item_id,
-    item_name,
-    category,
-    starting_bid,
-    auction_date::date as auction_date
-from {{ source('raw_data', 'item') }}
